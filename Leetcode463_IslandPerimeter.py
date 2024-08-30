5d0399b83b3ae873541468e93739bbebfe48e037
Thanh Vu <jordanvu@Thanhs-Laptop.local>
2024-08-29 20:25:23 -0700

    Initial commit

---
+++ Leetcode463_IslandPerimeter.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        perimeter = 0
+        rows, cols = len(grid), len(grid[0])
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i-1][j] == 1:
+                        perimeter -= 2
+                    if j > 0 and grid[i][j-1] == 1:
+                        perimeter -= 2
+        
+        return perimeter
+
+
+# Sample grid representing an island
+# 0 represents water, 1 represents land
+test_grid = [
+    [0, 1, 0, 0],
+    [1, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0]
+]
+if __name__ == '__main__':
+    solution = Solution()
+    result = solution.islandPerimeter(test_grid)
+    print(f'The perimeter of the island is: {result}')
