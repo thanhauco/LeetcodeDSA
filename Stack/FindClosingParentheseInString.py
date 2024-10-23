28cce2a50b0879d323336c5044fe907556accfa8
Jordan Minh Vu <thanhauco@gmail.com>
2024-10-22 20:04:47 -0700

    match parenthese

--- Stack/FindClosingParentheseInString.py
+++ Stack/FindClosingParentheseInString.py
@@ -1,3 +1,11 @@
+"""
+Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."
+
+Write a function that, given a sentence like the one above, along with the position of an opening parenthesis, finds the corresponding closing parenthesis.
+
+Example: if the example string above is input with the number 10 (position of the first parenthesis), the output should be 79 (position of the last parenthesis).
+"""
+
 def find_closing_parenthesis(sentence: str, opening_pos: int) -> int:
     # Check if the opening position is valid
     if sentence[opening_pos] != '(':
